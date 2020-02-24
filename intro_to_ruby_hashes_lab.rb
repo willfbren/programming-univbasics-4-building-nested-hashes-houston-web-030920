ded6d0c1d4fb5afbb007057d49db2492223c2431
Will Brennan <willfbren@gmail.com>
2020-02-24 20:51:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -35,7 +35,7 @@ def monopoly_with_third_tier
 	    :names => {
 	      :reading_railroad => {},
 	      :pennsylvania_railroad => {},
-	      :key3 => 3,
+	      :b_and_o_railroad => {},
 	      :key4 => 4
 	    }
 	  }
