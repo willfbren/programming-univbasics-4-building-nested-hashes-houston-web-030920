cecf276637d5cd56988f297597292e45cc7302b3
Will Brennan <willfbren@gmail.com>
2020-02-24 20:50:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,8 +29,8 @@ def monopoly_with_third_tier
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
 	      :two_pieces_owned => 50,
-	      :three_pieces_owned => 3,
-	      :key4 => 4
+	      :three_pieces_owned => 100,
+	      :four_pieces_owned => 200
 	    },
 	    :names => {
 	      :key1 => 1,
