553b11f6cc7240083bb12ff479e4e5630f7ef780
Will Brennan <willfbren@gmail.com>
2020-02-24 20:39:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -2,7 +2,7 @@ def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 	monopoly = {
-	  :key => 4
+	  :railroads => 4
 	}
 end
 
